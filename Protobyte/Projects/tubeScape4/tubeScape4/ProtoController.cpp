50bd7b8be8491f6a0777a6c8a7e4ba50a7686b7e
Ira Greenberg <irajgreenberg@gmail.com>
2019-08-13 12:45:06 -0500

    updates

---
+++ Protobyte/Projects/tubeScape4/tubeScape4/ProtoController.cpp
@@ -0,0 +1,188 @@
+/*
+Protobyte Library 0.1.7
+Ira Greenberg 2019
+*/
+
+#include "ProtoController.h"
+
+void ProtoController::init() {
+	//shadowsOn();
+	setLight(0, { -400, -200, 500 }, { 1, 1, 1 });
+	float theta{ 0.0f }, theta2{ 0 };
+	float r{ 240.0f };
+	//float theta2{ 0 };
+
+
+	plane = GroundPlane(Vec(0, 0, -100), Vec{ 0 }, Dim2f(5000, 3000), Col4{ .2, .2, .2, 1 }, 10, 10);
+	plane.setDiffuseMaterial(1);
+	plane.setSpecularMaterial(1);
+	plane.setShininess(15);
+
+	for (int i = 0; i < blockCount; i++) {
+		float h{};
+		if (i % 27 == 0) {
+			h = random(5, 875);
+		}
+		else if (i % 329 == 0) {
+			h = random(560, 945);
+		}
+		else {
+			h = random(30, 295);
+		}
+		blockModel = Block(
+				Vec(0,0,0),
+				Vec{0},
+				Dim3(1, 1, 1),
+				Col4(.5, .3, .25, 1)
+		);
+
+		blockModel.setDiffuseMaterial({ .25, .25, .25, 1 });
+		blockModel.setAmbientMaterial(0.0f);
+		//geoScape.at(i).setBumpMap("STG_Flesh/Normal_Maps/STG_Flesh_27-normal.jpg", .1f);
+		//geoScape.at(i).loadBumpMapTexture("vascular3_normal2.jpg");
+		blockModel.setTextureScale({ 1.0f, 1.0f });
+		blockModel.setSpecularMaterial(1);
+		blockModel.setShininess(70);
+
+		blockModel.setRegistration(Block::BOTTOM);
+
+
+
+		pos.push_back(Vec(
+			random(-1500, 1500),
+			0,
+			random(-1300, 500)
+		));
+		sz.push_back(Dim3(
+			random(20, 230),
+			h,
+			random(20, 230)
+		));
+	}
+
+	partModel = Particle(
+		Vec{0,0,0},
+		Vec{0,0,0},
+		Dim3f(1,1,1),
+		Particle::RECT,
+		"iraWrapped_low.jpg");
+		partModel.setSpeed(Vec(random(-2, 2), 0, random(-1, 1)));
+
+	// particles
+	for (int i = 0; i < particleCount; i++) {
+		partPos.push_back(Vec{ random(-400, 400), 900, random(-1000, 0) });
+		partSz.push_back(Dim3f(20, 20, 0));
+
+		
+		gravity.push_back(-.85);
+		damping.push_back(random(.55, .65));
+		friction.push_back(.565);
+	}
+
+}
+
+void ProtoController::run() {
+	for (int i = 0; i < particleCount; i++) {
+		parts.at(i).move();
+		/*parts.at(i).getSpeed().y += gravity.at(i);
+		parts.at(i).getPosition() += parts.at(i).getSpeed();*/
+		if (collide(parts.at(i))) {
+			parts.at(i).getSpeed().y *= -1;
+			parts.at(i).getSpeed().y *= damping.at(i);
+			parts.at(i).getSpeed().x *= friction.at(i);
+			parts.at(i).getSpeed().z *= friction.at(i);
+
+		}
+
+	}
+
+}
+
+bool ProtoController::collide(Particle& p) {
+	//trace("p = ", p);
+	for (int i = 0; i < blockCount; i++) {
+		if (p.getPosition().y < geoScape.at(i).getPosition().y + geoScape.at(i).getSize().h + p.getSize().h / 2 &&
+			p.getPosition().x > geoScape.at(i).getPosition().x - geoScape.at(i).getSize().w / 2 &&
+			p.getPosition().x < geoScape.at(i).getPosition().x + geoScape.at(i).getSize().w / 2 &&
+			p.getPosition().z > geoScape.at(i).getPosition().z - geoScape.at(i).getSize().d / 2 &&
+			p.getPosition().z < geoScape.at(i).getPosition().z + geoScape.at(i).getSize().d / 2 &&
+			p.getPosition().y > geoScape.at(i).getPosition().y + geoScape.at(i).getSize().h / 16) {
+
+			p.getPosition().y = geoScape.at(i).getPosition().y + geoScape.at(i).getSize().h + p.getSize().h / 2;
+
+			return true;
+		}
+		else if (p.getPosition().y < plane.getPosition().y + p.getSize().h / 2) {
+			p.getPosition().y = plane.getPosition().y + p.getSize().h / 2;
+			return true;
+		}
+
+		//	/*if (geoScape.at(i).getPosition().dist(p) < 125) {
+		//		return true;
+		//	}*/
+		//}
+		//return false;
+	}
+
+	//trace("p.getRadius()*2=", p.getRadius() * 2);
+		/*if (p.getPosition().y < plane.getPosition().y + p.getSize().h/2) {
+			p.getPosition().y = plane.getPosition().y + p.getSize().h/2;
+			return true;
+		}*/
+	return false;
+}
+
+void ProtoController::display() {
+	background(0);
+	arcBallBegin();
+
+	translate(0, -500, -400);
+
+	// ground plane
+	plane.display();
+
+	// geometric block
+	for (int i = 0; i < blockCount; i++) {
+		geoScape.at(i).display();
+	}
+
+	// particles
+	for (int i = 0; i < particleCount; i++) {
+		parts.at(i).display();
+
+		/*gravity.push_back(-.03);
+		damping.push_back(.925);
+		friction.push_back(.965);*/
+	}
+
+	arcBallEnd();
+}
+
+// Key and Mouse Events
+void ProtoController::keyPressed() {
+}
+
+void ProtoController::mousePressed() {
+}
+
+void ProtoController::mouseRightPressed() {
+}
+
+void ProtoController::mouseReleased() {
+}
+
+void ProtoController::mouseRightReleased() {
+}
+
+void ProtoController::mouseMoved() {
+}
+
+void ProtoController::mouseDragged() {
+}
+
+// Window Events
+void ProtoController::onResized() {
+}
+
+void ProtoController::onClosed() {
+}
---
+++ Protobyte/Projects/tubeScape4/tubeScape4/ProtoController.h
@@ -0,0 +1,68 @@
+/*
+Protobyte Library 0.1.7
+Ira Greenberg 2019
+*/
+
+#ifndef __PROTOCONTROLLER_H__
+#define __PROTOCONTROLLER_H__
+
+#include <iostream>
+#include "ProtoBaseApp.h"
+
+using namespace ijg;
+
+class ProtoController : public ProtoBaseApp {
+
+public:
+	void init();
+	void run();
+	void display();
+
+	// Key and Mouse Events
+	void keyPressed();
+	void mousePressed();
+	void mouseRightPressed();
+	void mouseReleased();
+	void mouseRightReleased();
+	void mouseMoved();
+	void mouseDragged();
+
+	// Window Events
+	void onResized();
+	void onClosed();
+
+	int tubeCount{ 1 };
+	std::vector<Spline3> splines;
+	std::vector<Tube> tubes;
+
+	int blockCount{ 300 };
+	std::vector<ProtoBlock> geoScape;
+
+	std::vector<Vec> pos;
+	std::vector<Dim3> sz;
+	GroundPlane plane;
+
+	// particles and physics
+	int particleCount{ 100 };
+	vect<Vec> particles;
+	float particleRadius{ 200 };
+	vect<Vec> speed;
+	vect<float> damping;
+	vect<float> gravity;
+	vect<float> friction;
+	bool collide(Particle& p);
+	vect<Particle> parts;
+
+	Block blockModel;
+	Particle partModel;
+	std::vector<Vec> partPos;
+	std::vector<Dim3> partSz;
+
+
+
+
+
+
+};
+
+#endif //__PROTOCONTROLLER_H__
