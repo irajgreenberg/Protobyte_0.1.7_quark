3d847636c50745f7625ef00804eed4a9fc547927
Ira Greenberg <irajgreenberg@gmail.com>
2019-10-13 22:26:09 -0500

    tubeSelfPortrait

---
+++ Protobyte/Projects/tubeSelfPortrait/tubeSelfPortrait/ProtoController.h
@@ -0,0 +1,41 @@
+/*
+Protobyte Library 0.1.7
+Ira Greenberg 2019
+*/
+
+#ifndef __PROTOCONTROLLER_H__
+#define __PROTOCONTROLLER_H__
+
+#include <iostream>
+#include "ProtoBaseApp.h"
+
+using namespace ijg;
+
+class ProtoController : public ProtoBaseApp {
+
+public:
+	void init();
+	void run();
+	void display();
+
+	// Key and Mouse Events
+	void keyPressed();
+	void mousePressed();
+	void mouseRightPressed();
+	void mouseReleased();
+	void mouseRightReleased();
+	void mouseMoved();
+	void mouseDragged();
+
+	// Window Events
+	void onResized();
+	void onClosed();
+
+	int pts{ 30 };
+	std::vector<Vec3>vs;
+	Spline s1;
+	Tube tube;
+
+};
+
+#endif //__PROTOCONTROLLER_H__
