1fd7b91b795ceb8a5e2c02f40a62fa5e3a356ba4
Greenberg <igreenberg@smu.edu>
2019-10-17 13:26:31 -0500

    new tube study

--- Protobyte/Projects/tubeSelfPortrait/tubeSelfPortrait/ProtoController.cpp
+++ Protobyte/Projects/tubeSelfPortrait/tubeSelfPortrait/ProtoController.cpp
@@ -12,7 +12,7 @@ void ProtoController::init() {
 	float theta2{ 0 };
 	for (int i = 0; i < pts; i++) {
 		r = (i * 2) + 250 - abs(sin(theta2) * 60);
-		vs.push_back(Vec(sin(theta) * r, -370 + i * 23, cos(theta) * r));
+		vs.push_back(Vec(sin(theta) * r, cos(-theta2) * -370 + i * 23, cos(theta) * r));
 
 		//vs.push_back({ cos(theta) * r, sin(theta) * r, 0 });
 		//theta += TWO_PI * 6 / pts;
@@ -22,15 +22,15 @@ void ProtoController::init() {
 
 
 	s1 = Spline(vs, 8, false);
-	tube = Tube(s1, 8, 18, false, "STG_Flesh/Diffuse_Maps/STG_Flesh_27-diffuse.jpg");
-	tube.setTransFuncObj(ProtoTransformFunction(ProtoTransformFunction::SINUSOIDAL, Tup2f{ 2, 274 }, 1));
+	tube = Tube(s1, 3, 12, false, "yellow.jpg");
+	tube.setTransFuncObj(ProtoTransformFunction(ProtoTransformFunction::SINUSOIDAL_RANDOM, Tup2f{ 20, 20 }, 2));
 	tube.setPerturbation({ random(3.2), random(1.5), random(1.2) });
 	//tube.setColor({ .1, 0, 0, 1 });
 	//tube.setDiffuseMaterial(1);
 	////tube.setAmbientMaterial(0.05f);
 	tube.setBumpMap("STG_Flesh/Normal_Maps/STG_Flesh_27-normal.jpg", .1f);
 	////tube.loadBumpMapTexture("vascular3_normal2.jpg");
-	tube.setTextureScale({ 1.0f, 0.03f });
+	tube.setTextureScale({ 1.0f, 0.3f });
 	tube.setSpecularMaterial(1);
 	tube.setShininess(125);
 }
@@ -40,7 +40,7 @@ void ProtoController::run() {
 
 void ProtoController::display() {
 	//background("iraWrapped_low.jpg");
-	background(1, 1, 0);
+	background(.35, .35, .385);
 	arcBallBegin();
 	//tube.display(WIREFRAME);
 	tube.display();
--- Protobyte/Projects/tubeSelfPortrait/tubeSelfPortrait/ProtoController.h
+++ Protobyte/Projects/tubeSelfPortrait/tubeSelfPortrait/ProtoController.h
@@ -31,7 +31,7 @@ public:
 	void onResized();
 	void onClosed();
 
-	int pts{ 30 };
+	int pts{ 130 };
 	std::vector<Vec3>vs;
 	Spline s1;
 	Tube tube;
