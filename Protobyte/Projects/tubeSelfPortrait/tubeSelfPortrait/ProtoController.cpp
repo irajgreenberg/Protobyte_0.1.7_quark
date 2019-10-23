aed8bdafd5c21ba372de5eed46f43c9c11d9aca2
irajgreenberg <irajgreenberg@gmail.com>
2019-10-22 21:36:34 -0500

    more work on Tube Portrait01

--- Protobyte/Projects/tubeSelfPortrait/tubeSelfPortrait/ProtoController.cpp
+++ Protobyte/Projects/tubeSelfPortrait/tubeSelfPortrait/ProtoController.cpp
@@ -9,7 +9,7 @@ Ira Greenberg 2019
 #include "ProtoController.h"
 
 void ProtoController::init() {
-	setLight(0, { 900, 0, 400 }, { 1, .6, 1 });
+	setLight(0, { 900, 0, 400 }, { 1 });
 	float radius{ 400.0f };
 	for (int i = 0; i < pts; i++) {
 		//r = (i * 2) + 250 - abs(sin(theta2) * 60);
@@ -19,22 +19,22 @@ void ProtoController::init() {
 		float x = sqrt(1.0 - u * u) * cos(t);
 		float y = sqrt(1.0 - u * u) * sin(t);
 		float z = u;
-
-		vs.push_back({ x * radius, y * radius, z * radius });
+		float a = random(radius * .25);
+		vs.push_back({ x * radius+a, y * radius + a, z * radius + a });
 	}
 
 
 	s1 = Spline(vs, 8, false);
 
 	tube = Tube(s1, 5, 12, ProtoTransformFunction(ProtoTransformFunction::SINUSOIDAL,
-		Tup2(.35, 19.5), 30), false, "yellow.jpg");
+		Tup2(.15, 8.5), 460), false, "gold01.jpg");
 	//tube.setPerturbation({ random(3.2), random(1.5), random(1.2) });
-	//tube.setColor({ .1, 0, 0, 1 });
+	tube.setColor({ .2, .2, .05, 1 });
 	//tube.setDiffuseMaterial(1);
 	////tube.setAmbientMaterial(0.05f);
-	tube.setBumpMap("STG_Flesh/Normal_Maps/STG_Flesh_27-normal.jpg", .5f);
+	tube.setBumpMap("STG_Flesh/Normal_Maps/STG_Flesh_27-normal.jpg", 1.5f);
 	////tube.loadBumpMapTexture("vascular3_normal2.jpg");
-	tube.setTextureScale({ 1.0f, 0.03f });
+	tube.setTextureScale({ .9f, 0.003f });
 	tube.setSpecularMaterial(1);
 	tube.setShininess(125);
 }
